d18ed212cd2df18082ce505ac86d0123c3e1f3d8
Carla Fernanda <alu0101278353@ull.edu.es>
2021-04-18 17:58:19 +0100

    nevos alimentos

--- tests/aliment/aliment.spec.ts
+++ tests/aliment/aliment.spec.ts
@@ -3,30 +3,35 @@ import {expect} from 'chai';
 import {Fruit} from "../../src/aliment/fruit";
 import {Vegetable} from "../../src/aliment/vegetable";
 import {Aliment} from "../../src/aliment/aliment";
+import {Cereal} from "../../src/aliment/cereal";
+import {Seed} from "../../src/aliment/seed";
+import {Fish} from "../../src/aliment/fish";
+import {Legume} from "../../src/aliment/legume";
+import {Meat} from "../../src/aliment/meat";
 
 
 describe(`Aliment Test`, () => {
-  const aliment3 = new Fruit("lechuga", 1, 14, 1.34, 0.2, 15, 1.2, 3, 5, 2, 'asd',"Barcelona")
-  /*const aliment9 = new Vegetable("lechuga", 1, 14, 1.34, 0.2, 15, 1.2, 3, 5, 2, "Barcelona");
-   const aliment4 = new Alimento("bizcocho", "varios", [426,4.5,18.6], 3, "Barcelona");
-  const aliment10 = new Alimento("avena", "cereales", [361,11.7,7.1], 1, "Tenerife");
-  const aliment4 = new Alimento("Donuts", "varios", [426,6.1,23], 5, "Madrid");
-  const aliment5 = new Alimento("carne de vaca", "carnes", [351,8,2.2], 12, "Barcelona");
-  const aliment6 = new Alimento("pollo", "carnes", [351,8,2.2], 6, "Barcelona");
-  const aliment7 = new Alimento("cebolla", "verduras", [351,8,2.2], 2, "Barcelona");
-  const aliment8 = new Alimento("tomate", "verduras", [22,1,0.11], 4, "Barcelona");
+  const manzana = new Fruit("manzana", 1, 14, 1.34, 0.2, 15, 1.2, 3, 5, 2, 'España',"Barcelona")
+  const avena = new Cereal("avena", 1, 11, 1.2, 0.5, 2, 1.8, 4, 5, 2, 'España',"Barcelona")
+  const donuts = new Cereal("donuts", 1, 11, 1.2, 0.5, 2, 1.8, 4, 5, 2, 'España',"Barcelona")
+  const bizcocho = new Cereal("bizcocho", 1, 11, 1.2, 0.5, 2, 1.8, 4, 5, 2, 'España',"Barcelona")
+  const lechuga = new Vegetable("lechuga", 1, 11, 1.2, 0.5, 2, 1.8, 4, 5, 2, 'España',"Barcelona")
+  const pasta = new Cereal("pasta", 1, 11, 1.2, 0.5, 2, 1.8, 4, 5, 2, 'España',"Barcelona")
+  const cereza = new Fruit("ceraza", 1, 11, 1.2, 0.5, 2, 1.8, 4, 5, 2, 'España',"Barcelona")
+  const carne_cordero = new Meat("carne de cordero", 1, 11, 1.2, 0.5, 2, 1.8, 4, 5, 2, 'España',"Barcelona")
+  const nata = new Cereal("avena", 2, 11, 1.2, 0.5, 2, 1.8, 4, 5, 2, 'España',"Barcelona")
+  const pan_rallado = new Cereal("pan rallado", 1, 11, 1.2, 0.8, 2, 1.8, 4, 5, 2, 'España',"Barcelona")
+  const pasta_integral = new Cereal("pasta integral", 1, 11, 2.2, 0.5, 2, 2, 4, 5, 2, 'España',"Barcelona")
+  const arroz_integral = new Cereal("arroz integral", 1, 11, 1.9, 0.5, 2, 9, 4, 5, 2, 'España',"Barcelona")
+  const cane_conejo = new Meat("carne de conejo", 1, 11, 1.2, 0.2, 2, 1.8, 4, 5, 2, 'España',"Barcelona")
 
-  const aliment1 = new Alimento("pasta", "varios", [375,12,1.8], 0.66, "Barcelona");
-  const aliment2 = new Alimento("arroz", "granos", [351,8,2.2], 2, "Barcelona");*/
+
 
   it('obteniendo el nombre del alimento', () => {
-    expect(aliment3).to.ownProperty("name");
-  });
- /* it('obteniendo informacion del alimento', () => {
-    expect(aliment3.printAliment()).to.be.equal("alimento:arroz, precio:1.9 cantidad de proteianas:351, cantidad de lipidos: 8, cantidad de carbohidratos: 2");
+    expect(nata).to.ownProperty("name");
   });
 
   it('obteniendo información del alimento lechuga', () => {
-    expect(aliment9.printAliment()).to.be.equal("alimento:lechuga, precio:1 cantidad de proteianas:14, cantidad de lipidos: 1.34, cantidad de carbohidratos: 0.2");
-  });*/
+    expect(lechuga.printAliment()).to.be.equal("alimento:lechuga, precio:1 cantidad de proteianas:14, cantidad de lipidos: 1.34, cantidad de carbohidratos: 0.2");
+  });
 });
